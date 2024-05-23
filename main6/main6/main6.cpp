b985805cc4457522a40811ef0cb59fe53c360458
Phraysser <167037455+Phraysser@users.noreply.github.com>
2024-05-24 00:19:00 +0300

    Add files via upload

---
+++ main6/main6/main6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    ifstream inputFile("file_f.txt", ios::binary);
+    ofstream outputFile("file_g.txt", ios::binary);
+
+    if (!inputFile.is_open() || !outputFile.is_open()) {
+        cerr << "Error opening files." << endl;
+        return 1;
+    }
+
+    outputFile << inputFile.rdbuf();
+
+    inputFile.close();
+    outputFile.close();
+
+    cout << "File copied successfully." << endl;
+
+    return 0;
+}
