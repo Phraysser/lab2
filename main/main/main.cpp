7eacef5daf78658176fc227748ac32c86edd054a
Phraysser <167037455+Phraysser@users.noreply.github.com>
2024-05-23 23:35:18 +0300

    Add files via upload

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -3,36 +3,35 @@
 using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
-    ifstream file("a.txt");
-    if (!file.is_open()) {
-        cout << "Ошибка открытия файла" << endl;
+    ifstream input("a.txt");
+    if (!input.is_open()) {
+        cout << "Ошибка открытия файла a.txt" << endl;
         return 1;
     }
 
-    int zeroCount = 0;
-    double negativeSum = 0.0;
-    double num;
+    ofstream output("a2.txt");
+    if (!output.is_open()) {
+        cout << "Ошибка открытия файла a2.txt" << endl;
+        return 1;
+    }
 
-    while (file >> num) {
-        if (num == 0) {
-            zeroCount++;
-        }
-        else if (num < 0) {
-            negativeSum += num;
+    double num;
+    while (input >> num) {
+        if (num != 0) {
+            output << num / 5 << " ";
         }
     }
 
-    file.close();
-
-    cout << "Содержимое файла a.txt:" << endl;
-    file.open("a.txt");
-    while (file >> num) {
+    input.close();
+    output.close();
+    
+    cout << "Содержимое файла a1.txt:" << endl;
+    input.open("a2.txt");
+    while (input >> num) {
         cout << num << " ";
     }
-    file.close();
-
-    cout << endl << "Количество нулевых элементов: " << zeroCount << endl;
-    cout << "Сумма отрицательных элементов: " << negativeSum << endl;
-
+    output.close();
+    cout << endl;
+    cout << "Ненулевые элементы файла a.txt успешно записаны в файл a2.txt с делением на 5" << endl;
     return 0;
 }
