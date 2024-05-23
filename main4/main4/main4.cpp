7cff8f5efa778e9d46ff4758586f034b52ca3092
Phraysser <167037455+Phraysser@users.noreply.github.com>
2024-05-23 23:57:37 +0300

    Add files via upload

---
+++ main4/main4/main4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    ifstream input("input.txt");
+    if (!input.is_open()) {
+        cerr << "Ошибка открытия файла input.txt" << endl;
+        return 1;
+    }
+
+    vector<int> numbers;
+    int num;
+    int position = 1;
+    while (input >> num) {
+        if (position % 2 == 0 && num > 0) {
+            numbers.push_back(num);
+        }
+        position++;
+    }
+
+    input.close();
+
+    if (numbers.empty()) {
+        cout << "В файле нет положительных чисел на четных позициях" << endl;
+        return 0;
+    }
+
+    int sum = 0;
+    for (int n : numbers) {
+        sum += n;
+    }
+
+    double average = static_cast<double>(sum) / numbers.size();
+
+    cout << "Среднее значение среди положительных чисел на четных позициях: " << average << endl;
+
+    return 0;
+}
